b21f1ee1d2807970ada8671c5d0a392d1e608873
soha-elghany <40267477+soha-elghany@users.noreply.github.com>
2020-05-04 11:43:38 +0100

    Add files via upload

--- Tableau Rest API login and csv.py
+++ Tableau Rest API login and csv.py
@@ -4,9 +4,6 @@
 # Sign-in to your site - using access token
 # Requests a list of all the workbooks (and owners) on the site
 # Writes this list out as a CSV file 
-# Manually login to your site and:
-
-pip install tableauserverclient
 
 import tableauserverclient as TSC 
 import pandas as pd
@@ -22,22 +19,21 @@ tableau_auth = TSC.PersonalAccessTokenAuth("Test", PAT, site_id=SITE)
 
 with server.auth.sign_in(tableau_auth):
 
-    request_options = TSC.RequestOptions(pagesize=500)
-    all_workbooks_items, pagination_item = server.workbooks.get(request_options)
-    workbook_id = [workbook.id for workbook in all_workbooks_items]
-    workbook_name = [workbook.name for workbook in all_workbooks_items]
-    url = [workbook.content_url for workbook in all_workbooks_items]
-    owner_id = [workbook.owner_id for workbook in all_workbooks_items]
-    project_name = [workbook.project_name for workbook in all_workbooks_items]
-    tags = [workbook.tags for workbook in all_workbooks_items]
-    description = [workbook.description for workbook in all_workbooks_items]
+    all_workbooks, pagination_item = server.workbooks.get()
+    workbook_data = [{"workbook_name": workbook.name,
+                      "size": workbook.size,
+                      "created_at":workbook.created_at,
+                      "owner_id":workbook.owner_id,
+                      "owner_name": server.users.get_by_id(workbook.owner_id).name}
+                     for workbook in all_workbooks]
     
-    print(workbook_name)
-    print(owner_id)
+    print(workbook_data)
     
 #saving into a csv file
-df = pd.DataFrame({'workbook name':workbook_name, 'owner ID':owner_id})
+df = pd.DataFrame.from_dict(workbook_data)
 df
 
 df.to_csv('workbook.csv', sep ='\t', header = True)
 df.to_csv(r'C://Users/Username/Documents/workbookx.csv') - put in the file path
+
+server.auth.sign_out()
